18aaab2bf9e69e65891e94c8427cdb33bd75f963
t-yaginuma <toru45s@gmail.com>
2024-08-03 21:41:03 +0900

    add TyFileUpload

---
+++ src/components/TyFileUpload/TyFileUpload.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type Props = {};
+
+export default function TyFileUpload(props: Props) {
+  // return<input type="file" name="test" required />
+  return <input type="file" name="test" />;
+}
