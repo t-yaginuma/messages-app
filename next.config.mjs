3d213c6fbc98846dfaaad7348bd73f0ca7bcd9ca
t-yaginuma <toru45s@gmail.com>
2024-08-03 21:37:28 +0900

    add global scss

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.jp",
+        port: "",
+      },
+    ],
+  },
+  sassOptions: {
+    prependData: `
+    @use '@/styles/_variables' as *; @use "sass:map";
+    `,
+  },
+};
 
 export default nextConfig;
