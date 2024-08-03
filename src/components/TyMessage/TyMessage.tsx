8193833c0e36b33abc753da8fd1206ce832b9988
t-yaginuma <toru45s@gmail.com>
2024-08-03 21:40:26 +0900

    add TyMEssage

---
+++ src/components/TyMessage/TyMessage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import TyProfile from "@/components/TyProfile/TyProfile";
+import dayjs from "dayjs";
+
+type Props = {
+  text: string;
+};
+
+export default function TyMessage(props: Props) {
+  const { text } = props;
+  return (
+    <article>
+      <TyProfile src="https://placehold.jp/32x32.png" name="Toru Yaginuma" />
+      {text}
+      <div>{dayjs().format()}</div>
+    </article>
+  );
+}
