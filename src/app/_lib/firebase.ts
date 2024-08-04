5924b36309b3f1e2e95805d44fd226897e1e6a6c
t-yaginuma <toru45s@gmail.com>
2024-08-04 21:01:02 +0900

    update storageを追加

--- src/app/_lib/firebase.ts
+++ src/app/_lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, signOut } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -18,4 +19,6 @@ const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 const auth = getAuth(app);
-export { db, auth };
+const storage = getStorage(app);
+
+export { db, auth, storage, signOut };
