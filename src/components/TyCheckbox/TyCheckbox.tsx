817072aad9d7af4a6c91dfcae3fdcd8322d4daaf
t-yaginuma <toru45s@gmail.com>
2024-08-04 21:02:40 +0900

    update TyCheckbox 設定を追加

--- src/components/TyCheckbox/TyCheckbox.tsx
+++ src/components/TyCheckbox/TyCheckbox.tsx
@@ -2,14 +2,24 @@ import React from "react";
 
 type Props = {
   label: string;
+  value: boolean;
+  isDisabled: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 export default function TyInput(props: Props) {
-  const { label } = props;
+  const { label, onChange, value, isDisabled } = props;
   return (
     <div>
-      <input type="checkbox" id="scales" name="scales" checked />
-      <label for="scales">{label}</label>
+      <input
+        type="checkbox"
+        id="check"
+        name="check"
+        disabled={isDisabled}
+        onChange={onChange}
+        checked={value}
+      />
+      <label htmlFor="check">{label}</label>
     </div>
   );
 }
