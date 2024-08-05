8c4e068fa8c5656d0975324cd018e95f8369d666
t-yaginuma <toru45s@gmail.com>
2024-08-06 08:24:26 +0900

    update TyInput エラー時の状態を追加

--- src/components/TyInput/TyInput.tsx
+++ src/components/TyInput/TyInput.tsx
@@ -1,12 +1,25 @@
+import classNames from "classnames";
 import React from "react";
-
+import styles from "@/components/TyInput/TyInput.module.scss";
 type Props = {
   type?: "text" | "email" | "password" | "date";
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
+  name?: string;
+  isRequired?: boolean;
+  isError?: boolean;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 export default function TyInput(props: Props) {
-  const { onChange, type = "text", value } = props;
-  return <input value={value} type={type} onChange={onChange} />;
+  const { onChange, type = "text", name, isRequired, isError, value } = props;
+  return (
+    <input
+      className={classNames({ [styles["ty-input--error"]]: isError })}
+      value={value}
+      type={type}
+      name={name}
+      onChange={onChange}
+      required={isRequired}
+    />
+  );
 }
