99e47e215de1e6218e78b3a46cfe3e6831850e49
t-yaginuma <toru45s@gmail.com>
2024-08-06 08:23:17 +0900

    add TyText

---
+++ src/components/TyText/TyText.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import style from "@/components/TyText/TyText.module.scss";
+import classNames from "classnames";
+import { isErrored } from "stream";
+
+type Props = {
+  children: React.ReactNode;
+  isError: boolean;
+};
+
+export default function TyText(props: Props) {
+  const { children, isError } = props;
+
+  return (
+    <p
+      className={classNames({
+        [style["ty-text"]]: true,
+        [style["ty-text--error"]]: isError,
+      })}
+    >
+      {children}
+    </p>
+  );
+}
