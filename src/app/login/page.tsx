6ae3a9df3bad9c59bcd6adf06b53388963e0fedd
t-yaginuma <toru45s@gmail.com>
2024-08-06 08:25:29 +0900

    update login画面のバリデーションを追加

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,57 +1,71 @@
 "use client";
 
-import React, { useState } from "react";
-
+import React, { useState, FormEventHandler } from "react";
 import TyInput from "@/components/TyInput/TyInput";
 import TyLabel from "@/components/TyLabel/TyLabel";
 import TyHeading from "@/components/TyHeading/TyHeading";
 import TyButton from "@/components/TyButton/TyButton";
 import TyForm from "@/components/TyForm/TyForm";
+import TyText from "@/components/TyText/TyText";
+import TyDiv from "@/components/TyDiv/TyDiv";
 import { auth } from "@/app/_lib/firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
-export default function Home() {
+export default function Login() {
   const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
-  const submit = async () => {
-    console.log("submit");
+  const submit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const email = (form.get("email") as string) || "";
+    const password = (form.get("password") as string) || "";
 
     await signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        console.log(user);
+        // const user = userCredential.user;
+        // console.log(user);
         router.push("/");
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
+        switch (errorCode) {
+          case "auth/invalid-credential":
+            setErrorMessage("メールアドレスか、パスワードが間違えています。");
+            break;
+        }
       });
   };
 
   return (
     <>
       <TyHeading type="h1" label="ログイン" />
-      <TyForm>
+      <TyForm onSubmit={submit}>
         <TyLabel label="メールアドレス">
           <TyInput
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
+            type="email"
+            name="email"
+            isRequired
+            isError={!!errorMessage}
           />
         </TyLabel>
         <TyLabel label="パスワード">
           <TyInput
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
+            type="password"
+            name="password"
+            isRequired
+            isError={!!errorMessage}
           />
         </TyLabel>
+        {errorMessage && <TyText isError>{errorMessage}</TyText>}
+        <TyButton type="submit" label="ログイン" />
       </TyForm>
-      <TyButton label="ログイン" onClick={submit} />
+      <TyDiv>
+        <Link href="/register">新規登録する</Link>
+      </TyDiv>
     </>
   );
 }
