b50c694d00eae38b0ac4c81c24a0378f5de5ec92
t-yaginuma <toru45s@gmail.com>
2024-08-07 09:33:08 +0900

    update 登録画面を実装

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -13,26 +13,43 @@ import TyButton from "@/components/TyButton/TyButton";
 import TyCheckbox from "@/components/TyCheckbox/TyCheckbox";
 import TyFileUpload from "@/components/TyFileUpload/TyFileUpload";
 import TyRadioGroup from "@/components/TyRadioGroup/TyRadioGroup";
+import TyForm from "@/components/TyForm/TyForm";
+import TyDiv from "@/components/TyDiv/TyDiv";
+import TyText from "@/components/TyText/TyText";
 
 import { db, auth, storage } from "@/app/_lib/firebase";
 import { collection, doc, setDoc, addDoc } from "firebase/firestore";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import TyTextLink from "@/components/TyTextLink/TyTextLink";
+import dayjs from "dayjs";
 
 export default function Register() {
   const router = useRouter();
-  const [userName, setUserName] = useState("");
-  const [mailAddress, setMailAddress] = useState("");
-  const [password, setPassword] = useState("");
-  const [birthday, setBirthday] = useState("");
   const [image, setImage] = useState<File | null>(null);
-  const [gender, setGender] = useState("1");
   const [isRead, setIsRead] = useState(false);
   const [isAgreed, setIsAgreed] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
-  const usersRef = collection(db, "users");
+  const submit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-  const submit = async () => {
+    const form = new FormData(e.currentTarget);
+    const userName = (form.get("user-name") as string) || "";
+    const mailAddress = (form.get("mail-address") as string) || "";
+    const password = (form.get("password") as string) || "";
+    const gender = (form.get("gender") as string) || "";
+    const birthday = (form.get("birthday") as string) || "";
+    const imagePost = form.get("imagePost") || "";
+
+    console.log(form);
+    console.log(userName);
+    console.log(mailAddress);
+    console.log(password);
+    console.log(gender);
+    console.log(birthday);
+    console.log(imagePost);
+
     createUserWithEmailAndPassword(auth, mailAddress, password)
       .then(async (userCredential) => {
         const uid = await userCredential.user.uid;
@@ -50,6 +67,7 @@ export default function Register() {
         return { uid, imageUrl };
       })
       .then((obj) => {
+        const usersRef = collection(db, "users");
         addDoc(usersRef, {
           user_name: userName,
           mail_address: mailAddress,
@@ -63,7 +81,11 @@ export default function Register() {
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        // ..
+        switch (errorCode) {
+          case "auth/invalid-credential":
+            setErrorMessage("メールアドレスか、パスワードが間違えています。");
+            break;
+        }
       });
   };
 
@@ -83,42 +105,35 @@ export default function Register() {
   ];
 
   useEffect(() => {
-    console.log(image?.name);
+    // console.log(image?.name);
   }, [image]);
 
   return (
     <>
       <TyHeading type="h1" label="ユーザー登録" />
-      <div>
+      <TyForm onSubmit={submit}>
         <TyLabel label="ユーザー名">
-          <TyInput
-            type="text"
-            value={userName}
-            onChange={(e) => {
-              setUserName(e.target.value);
-            }}
-          />
+          <TyInput type="text" name="user-name" placeholder="名前" isRequired />
         </TyLabel>
         <TyLabel label="メールアドレス">
           <TyInput
             type="email"
-            value={mailAddress}
-            onChange={(e) => {
-              setMailAddress(e.target.value);
-            }}
+            name="mail-address"
+            placeholder="メールアドレス"
+            isRequired
           />
         </TyLabel>
         <TyLabel label="パスワード">
           <TyInput
             type="password"
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
+            name="password"
+            placeholder="パスワード"
+            isRequired
           />
         </TyLabel>
         <TyLabel label="プロフィールアイコン">
           <TyFileUpload
+            name="image"
             onChange={(e) => {
               if (e.target.files && e.target.files[0]) {
                 setImage(e.target.files[0]);
@@ -129,53 +144,48 @@ export default function Register() {
         <TyLabel label="生年月日">
           <TyInput
             type="date"
-            value={birthday}
-            onChange={(e) => {
-              setBirthday(e.target.value);
-            }}
+            name="birthday"
+            maxDate={`${dayjs(new Date()).format("YYYY-MM-DD")}`}
+            isRequired
           />
         </TyLabel>
         <TyLabel label="性別">
           <TyRadioGroup
             options={genderOptions}
-            value={gender}
-            onChange={(e) => {
-              console.log(e);
-              setGender(e.target.value);
-            }}
+            name="gender"
+            isRequired
+            defaultValue="3"
           />
-          {gender}
         </TyLabel>
         <TyLabel label="利用規約への同意">
-          <Link
-            href="https://google.com"
-            target="_blank"
-            onClick={() => {
-              setIsRead(true);
-            }}
-          >
-            利用規約を読む
-          </Link>
+          <TyDiv>
+            <Link
+              href="https://google.com"
+              target="_blank"
+              onClick={() => {
+                setIsRead(true);
+              }}
+            >
+              利用規約を読む
+            </Link>
+          </TyDiv>
 
           <TyCheckbox
             label="同意する"
             value={isAgreed}
             isDisabled={!isRead}
             onChange={(e) => {
-              console.log(e);
               setIsAgreed(e.target.checked);
             }}
           ></TyCheckbox>
         </TyLabel>
 
-        <TyButton
-          label="キャンセル"
-          onClick={() => {
-            router.push("/login");
-          }}
-        />
-        <TyButton label="登録" onClick={submit} isDisabled={!isAgreed} />
-      </div>
+        <TyButton type="submit" label="登録" isDisabled={!isAgreed} />
+      </TyForm>
+      {errorMessage && <TyText isError>{errorMessage}</TyText>}
+      <TyDiv>
+        <TyTextLink href="/login" text="ログイン画面へ戻る" />
+      </TyDiv>
     </>
   );
 }
