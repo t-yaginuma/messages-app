85766b28778132be2f7d219fc04fff7ebff841d7
t-yaginuma <toru45s@gmail.com>
2024-08-07 09:32:48 +0900

    update TyFIleUpload propsを追加

--- src/components/TyFileUpload/TyFileUpload.tsx
+++ src/components/TyFileUpload/TyFileUpload.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 
 type Props = {
+  name?: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 export default function TyFileUpload(props: Props) {
-  const { onChange } = props;
+  const { onChange, name } = props;
 
   return (
     <input
       type="file"
-      name="image"
       accept=".png,.jpeg,.jpg"
       onChange={onChange}
+      name={name}
     />
   );
 }
