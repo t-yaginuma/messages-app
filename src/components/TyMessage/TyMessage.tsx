cb98f327f8b5b81ea8521469ebc1d5190f86f73d
t-yaginuma <toru45s@gmail.com>
2024-08-07 09:31:59 +0900

    update TyMessage データを繋ぎこみ

--- src/components/TyMessage/TyMessage.tsx
+++ src/components/TyMessage/TyMessage.tsx
@@ -1,18 +1,38 @@
 import React from "react";
 import TyProfile from "@/components/TyProfile/TyProfile";
 import dayjs from "dayjs";
+import TyTextLink from "../TyTextLink/TyTextLink";
+import { doc, deleteDoc } from "firebase/firestore";
+import { db } from "@/app/_lib/firebase";
 
 type Props = {
+  profile_icon: string;
+  date: string;
+  user_name: string;
   text: string;
+  id: string;
+  isDeleteVisible: boolean;
+  onDelete: (e: React.MouseEvent<HTMLAnchorElement>, postId: string) => {};
 };
 
 export default function TyMessage(props: Props) {
-  const { text } = props;
+  const { id, user_name, date, profile_icon, text, isDeleteVisible, onDelete } =
+    props;
+
   return (
     <article>
-      <TyProfile src="https://placehold.jp/32x32.png" name="Toru Yaginuma" />
+      <TyProfile image={profile_icon} name={user_name} />
       {text}
-      <div>{dayjs().format("YYYY-MM-DD hh:mm")}</div>
+      <div>{dayjs(date).format("YYYY-MM-DD hh:mm")}</div>
+      {isDeleteVisible && (
+        <TyTextLink
+          text="削除"
+          href="#"
+          onClick={(e) => {
+            onDelete(e, id);
+          }}
+        ></TyTextLink>
+      )}
     </article>
   );
 }
