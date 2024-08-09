c9f278d6aa579179e859195db74bf066d085ef1c
t-yaginuma <toru45s@gmail.com>
2024-08-09 09:51:37 +0900

    update TyHeading type を追加

--- src/components/TyHeading/TyHeading.tsx
+++ src/components/TyHeading/TyHeading.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import styles from "@/components/TyHeading/TyHeading.module.scss";
+import classNames from "classnames";
 
 type Props = {
   type: "h1" | "h2" | "h3";
@@ -7,5 +9,29 @@ type Props = {
 
 export default function TyHeading(props: Props) {
   const { type, label } = props;
-  return <h1>{label}</h1>;
+  const contents = {
+    h1: (
+      <h1
+        className={classNames(styles["ty-heading"], styles["ty-heading--h1"])}
+      >
+        {label}
+      </h1>
+    ),
+    h2: (
+      <h2
+        className={classNames(styles["ty-heading"], styles["ty-heading--h2"])}
+      >
+        {label}
+      </h2>
+    ),
+    h3: (
+      <h3
+        className={classNames(styles["ty-heading"], styles["ty-heading--h3"])}
+      >
+        {label}
+      </h3>
+    ),
+  };
+
+  return contents[type];
 }
