2efa935bc6bd77e5c41be97b1fd9e4e1ce5451dc
t-yaginuma <toru45s@gmail.com>
2024-08-09 09:51:17 +0900

    add TyLabel

--- src/components/TyLabel/TyLabel.tsx
+++ src/components/TyLabel/TyLabel.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "@/components/TyLabel/TyLabel.module.scss";
 
 type Props = {
   label: string;
@@ -8,9 +9,9 @@ type Props = {
 export default function TyInput(props: Props) {
   const { label, children } = props;
   return (
-    <div>
-      <span>{label}</span>
-      <div>{children}</div>
-    </div>
+    <label className={styles["ty-label"]}>
+      <span className={styles["ty-label__label"]}>{label}</span>
+      <div className={styles["ty-label__content"]}>{children}</div>
+    </label>
   );
 }
